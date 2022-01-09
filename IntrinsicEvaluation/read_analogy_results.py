980efe6b0e03a2836b5a191fdca7469730b9dfa2
rumesh <rumesh.m.s16@gmail.com>
2022-01-09 19:09:18 +0530

    reformat code

--- IntrinsicEvaluation/read_analogy_results.py
+++ IntrinsicEvaluation/read_analogy_results.py
@@ -2,7 +2,8 @@ __author__ = 'kjtdi'
 
 import codecs
 
-# This file is used to get the final averaged score for each relationship type, Derivational, Encyclopedic and Inflectional
+# This file is used to get the final averaged score for each relationship type, Derivational, Encyclopedic and
+# Inflectional
 file_to_read = codecs.open("./Analogy Results/test_fasttext_300_pretrained.txt", encoding='utf-8', errors='ignore')
 
 total_correct_i = 0
@@ -26,66 +27,66 @@ first_line = False
 for line in file_to_read:
     line = line.rstrip()
 
-    if (line.startswith("I")):
+    if line.startswith("I"):
         print(line)
         first_line = False
         i_start = True
         correct = 0
         qs = 0
 
-    if (line.startswith("D")):
+    if line.startswith("D"):
         print(line)
         first_line = False
         d_start = True
         correct = 0
         qs = 0
 
-    if (line.startswith("E")):
+    if line.startswith("E"):
         print(line)
         first_line = False
         e_start = True
         correct = 0
         qs = 0
 
-    if (i_start and line.isdigit() and not (first_line)):
+    if i_start and line.isdigit() and not first_line:
         first_line = True
         correct = int(line)
         total_correct_i += correct
         continue
 
-    if (i_start and line.isdigit() and first_line):
+    if i_start and line.isdigit() and first_line:
         qs = int(line)
         total_qs_i += qs
         print(round((correct / qs) * 100, 2))
         continue
 
-    if (d_start and line.isdigit() and not (first_line)):
+    if d_start and line.isdigit() and not first_line:
         first_line = True
         correct = int(line)
         total_correct_d += correct
         # print("D Correct", correct)
         continue
 
-    if (d_start and line.isdigit() and first_line):
+    if d_start and line.isdigit() and first_line:
         qs = int(line)
         total_qs_d += qs
         print(round((correct / qs) * 100, 2))
         continue
 
-    if (e_start and line.isdigit() and not (first_line)):
+    if e_start and line.isdigit() and not first_line:
         first_line = True
         correct = int(line)
         total_correct_e += correct
         # print("E Correct", correct)
         continue
 
-    if (e_start and line.isdigit() and first_line):
+    if e_start and line.isdigit() and first_line:
         qs = int(line)
         total_qs_e += qs
         print(round((correct / qs) * 100, 2))
         continue
 
-    if (line == ""):
+    if line == "":
         i_start = False
         d_start = False
         e_start = False
